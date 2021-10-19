c8c677a67653f37e78c0f903ce09fd8cb96755e1
jflanakin-at-jamf <july.flanakin@jamf.com>
2021-10-19 09:17:49 -0500

    add and rename scripts

---
+++ Linux-Scripts/ubuntu-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ufw allow ssh
+ufw enable
+ufw reload
+apt update && apt upgrade -y
+apt install intel-microcode build-essential dkms linux-headers-$(uname -r)
+apt install sudo vim open-vm-tools openssh-server ufw molly-guard fail2ban dnsutils net-tools
+apt install gcc make check git tar gzip wget curl rsync nmon htop tmux neofetch zsh bpytop
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+

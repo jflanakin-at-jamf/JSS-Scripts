8313252757302512077dff79eb72d417e8883942
jflanakin-at-jamf <july.flanakin@jamf.com>
2021-10-19 09:23:51 -0500

    Update ubuntu-install.sh

--- Linux-Scripts/ubuntu-install.sh
+++ Linux-Scripts/ubuntu-install.sh
@@ -3,9 +3,9 @@ ufw allow ssh
 ufw enable
 ufw reload
 apt update && apt upgrade -y
-apt install intel-microcode build-essential dkms linux-headers-$(uname -r)
-apt install sudo vim open-vm-tools openssh-server ufw molly-guard fail2ban dnsutils net-tools
-apt install gcc make check git tar gzip wget curl rsync nmon htop tmux neofetch zsh bpytop
+apt install intel-microcode build-essential dkms linux-headers-$(uname -r) -y
+apt install sudo vim open-vm-tools openssh-server ufw molly-guard fail2ban dnsutils net-tools -y
+apt install gcc make check git tar gzip wget curl rsync nmon htop tmux neofetch zsh bpytop -y
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 exit 0
